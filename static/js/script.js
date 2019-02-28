55d4a1324d77f22d723369dd37721829230d948e
IoanHadarean <42572834+IoanHadarean@users.noreply.github.com>
2019-02-28 03:41:05 -0800

    Added toggle for radio buttons/added styling for nav checkbox

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,7 @@ var servicesImage = document.getElementsByClassName("services__image")[0];
 var servicesDarken = document.getElementsByClassName("services__darken")[0];
 var navItems = document.getElementsByClassName("nav__items")[0];
 var navBox = document.getElementsByClassName("nav__box")[0];
-
+var navCheckbox = document.getElementsByClassName("nav__checkbox")[0];
 
 
 // Function to hide servicesHeader, servicesImage and servicesDarken only below 768px
@@ -25,19 +25,21 @@ var navBox = document.getElementsByClassName("nav__box")[0];
 
 // Function for toggling menu on mobile
 toggleMenu.addEventListener('click', function (e) {
-    if (navItems.style.display === 'none' && window.innerWidth <= 768) {
+    if (navItems.style.display === 'none') {
         navItems.classList.add('nav__items--active');
         navBox.classList.add('nav__box--active');
         navItems.style.display = 'block';
         navBox.style.display = 'block';
+        navCheckbox.style.display = 'block';
         toggleMenu.style.transform = 'rotate(180deg)';
         servicesHeader.style.visibility = "hidden";
         servicesImage.style.visibility = "hidden";
-        servicesDarken.style.visibility = "hidden";
+        servicesDarken.style.visibility = "hidden";    
     }
     else {
         navItems.style.display = 'none';
         navBox.style.display = 'none';
+        navCheckbox.style.display = 'none';
         navItems.classList.remove('nav__items--active');
         navBox.classList.remove('nav__box--active');
         toggleMenu.style.transform = 'rotate(90deg)';
